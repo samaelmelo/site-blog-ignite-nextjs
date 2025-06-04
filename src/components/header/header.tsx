c5f4b58a6e46e7627cef924e6e87e2e285dfd1d4
Samael Melo <samaelmelo.dev@gmail.com>
2025-06-04 17:31:22 -0400

    landing page finished

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,7 +5,7 @@ import { Logo } from '../logo';
 
 export const Header = () => {
   return (
-    <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blue supports-[backdrop-filters]:bg-background/60">
+    <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filters]:bg-background/60">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-80">
         <div className="flex h-16 items-center justify-between">
           <Logo/>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
-import { FeatureSection } from "@/components/feature-section";
-import { HeroSection } from "@/components/hero-section";
+import { CallToAction } from '@/components/call-to-action';
+import { CustomerStorySection } from '@/components/customer-story-section';
+import { FeatureSection } from '@/components/feature-section';
+import { HeroSection } from '@/components/hero-section';
+import { SupportSection } from '@/components/support-section';
 
 export default function Home() {
   return (
     <>
       <article className="flex flex-col">
-        <HeroSection/>
-        <FeatureSection/>
+        <HeroSection />
+        <FeatureSection />
+        <SupportSection />
+        <CustomerStorySection />
+        <CallToAction />
       </article>
     </>
   );
